2cfd401a8e0dda7a01c0e23bed50491f5096c804
romlilee <nikolasanelka67@gmail.com>
2023-06-18 18:26:03 +0800

    master

--- resources/views/pages/ujian/mulai.blade.php
+++ resources/views/pages/ujian/mulai.blade.php
@@ -5,42 +5,41 @@
 @section('content')
 <!-- Style untuk timer-->
 <style>
-    h1{
-        color: #001e81;
-        font-weight: 100;
-        font-size: 40px;
-        margin: 40px 0px 20px;
-    }
+  h1 {
+    color: #001e81;
+    font-weight: 100;
+    font-size: 40px;
+    margin: 40px 0px 20px;
+  }
 
-    #clockdiv{
-        font-family: sans-serif;
-        color: #fff;
-        display: inline-block;
-        font-weight: 100;
-        text-align: center;
-        font-size: 25px;
-    }
+  #clockdiv {
+    font-family: sans-serif;
+    color: #fff;
+    display: inline-block;
+    font-weight: 100;
+    text-align: center;
+    font-size: 25px;
+  }
 
-    #clockdiv > div{
-        padding: 10px;
-        border-radius: 3px;
-        background: #00d9ff;
-        display: inline-block;
-    }
+  #clockdiv>div {
+    padding: 10px;
+    border-radius: 3px;
+    background: #00d9ff;
+    display: inline-block;
+  }
 
-    #clockdiv div > span{
-        padding: 15px;
-        border-radius: 3px;
-        background: #001e81;
-        display: inline-block;
-    }
+  #clockdiv div>span {
+    padding: 15px;
+    border-radius: 3px;
+    background: #001e81;
+    display: inline-block;
+  }
 
-    .smalltext{
-        padding-top: 5px;
-        font-size: 16px;
-        color: black;
-    }
-    
+  .smalltext {
+    padding-top: 5px;
+    font-size: 16px;
+    color: black;
+  }
 </style>
 <!-- Header -->
 <div class="header bg-primary pb-6">
@@ -70,59 +69,65 @@
         <!-- Card header -->
         <div class="card-header">
           <div class="row">
-            <div class="col-sm-4"><h3 class="mb-0">Nama Mata Pelajaran: {{ $mapel->nama_mapel }}</h3></div>
-            <div class="col-sm-4"><h3 class="text-center">Jumlah Soal: {{$soal1}}</h3></div>
-            <div class="col-sm-4"><h3 class="text-right">Waktu Ujian: {{ $selisi->selisih_menit }} Menit</h3></div>
+            <div class="col-sm-4">
+              <h3 class="mb-0">Nama Mata Pelajaran: {{ $mapel->nama_mapel }}</h3>
+            </div>
+            <div class="col-sm-4">
+              <h3 class="text-center">Jumlah Soal: {{$soal1}}</h3>
+            </div>
+            <div class="col-sm-4">
+              <h3 class="text-right">Waktu Ujian: {{ $selisi->selisih_menit }} Menit</h3>
+            </div>
           </div>
 
 
         </div>
         <!-- Pemanggilan timer -->
         <div class="row mb-3" id="clockdiv" data-end-time="{{ $selisi->selisih_menit }}">
-            <h1 class="text-center">Ujian akan berakhir pada:</h1>
-            <div>
-                <span class="hours"></span>
-                <div class="smalltext">Jam</div>
-            </div>
-            <div>
-                <span class="minutes"></span>
-                <div class="smalltext">Menit</div>
-            </div>
-            <div>
-                <span class="seconds"></span>
-                <div class="smalltext">Detik</div>
-            </div>
+          <h1 class="text-center">Ujian akan berakhir pada:</h1>
+          <div>
+            <span class="hours"></span>
+            <div class="smalltext">Jam</div>
+          </div>
+          <div>
+            <span class="minutes"></span>
+            <div class="smalltext">Menit</div>
+          </div>
+          <div>
+            <span class="seconds"></span>
+            <div class="smalltext">Detik</div>
+          </div>
         </div>
         <div class="card-body">
-            <form action="/kirim-jawaban" method="POST">
-                @csrf
-                <input type="hidden" name="id_mapel" value="{{$mapel->id}}" readonly>
-                @foreach ($soal as $key => $value)
-                <input type="hidden" name="id_soal{{$key+1}}" value="{{$value->id}}">
-                <input type="hidden" name="jawaban{{$key+1}}" value="0">
-                <div class="card mt-5">
-                    <div class="card-header">
-                        Pertanyaan {{$key+1}}
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">
-                            {!! $value->nama_soal !!}
-                        </h5>
-                        @foreach ($value->opsi as $op)
-                        <li class="ml-5">
-                            <input type="radio" name="jawaban{{$key+1}}" value="{{$op->nama_opsi}}">
-                            {{$op->nama_opsi}}
-                        </li>
-                        @endforeach
-                        <input type="hidden" name="index" value="<?php echo $key+1 ?>">
-                        <input type="hidden" name="id_soal" value="{{ $value->id }}">
-                    </div>
-                </div>
+          <form action="/kirim-jawaban" method="POST">
+            @csrf
+            <input type="hidden" name="id_mapel" value="{{$mapel->id}}" readonly>
+            @foreach ($soal as $key => $value)
+            <input type="hidden" name="id_soal{{$key+1}}" value="{{$value->id}}">
+            <input type="hidden" name="jawaban{{$key+1}}" value="0">
+            <div class="card mt-5">
+              <div class="card-header">
+                Pertanyaan {{$key+1}}
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">
+                  {!! $value->nama_soal !!}
+                </h5>
+                @foreach ($value->opsi as $op)
+                <li class="ml-5">
+                  <input type="radio" name="jawaban{{$key+1}}" value="{{$op->nama_opsi}}" required>
+                  {{$op->nama_opsi}}
+                </li>
                 @endforeach
-                <div class="modal-footer">
-                    <button type="submit" name="submit" id="submit" class="btn btn-primary">Selesai</button>
-                </div>
-            </form>
+                <input type="hidden" name="index" value="<?php echo $key + 1 ?>">
+                <input type="hidden" name="id_soal" value="{{ $value->id }}">
+              </div>
+            </div>
+            @endforeach
+            <div class="modal-footer">
+              <button type="submit" name="submit" id="submit" class="btn btn-primary">Selesai</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
@@ -131,51 +136,50 @@
 @endsection
 @push('addon-script')
 <script type="text/javascript">
-   // Fitur timer (hasil copas awkwkkwk)
+  // Fitur timer (hasil copas awkwkkwk)
   function getTimeRemaining(endtime) {
-  const total = Date.parse(endtime) - Date.parse(new Date());
-  const seconds = Math.floor((total / 1000) % 60);
-  const minutes = Math.floor((total / 1000 / 60) % 60);
-  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-  
-  return {
-    total,
-    hours,
-    minutes,
-    seconds
-  };
-}
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
 
-function initializeClock(id, endtime) {
-  const clock = document.getElementById(id);
-  const hoursSpan = clock.querySelector('.hours');
-  const minutesSpan = clock.querySelector('.minutes');
-  const secondsSpan = clock.querySelector('.seconds');
-
-  function updateClock() {
-    const t = getTimeRemaining(endtime);
-
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
-    }
+    return {
+      total,
+      hours,
+      minutes,
+      seconds
+    };
   }
 
-  updateClock();
-  const timeinterval = setInterval(updateClock, 1000);
-}
+  function initializeClock(id, endtime) {
+    const clock = document.getElementById(id);
+    const hoursSpan = clock.querySelector('.hours');
+    const minutesSpan = clock.querySelector('.minutes');
+    const secondsSpan = clock.querySelector('.seconds');
 
-const endTime = new Date(parseInt(document.getElementById('clockdiv').getAttribute('data-end-time')));
-const deadline = new Date(Date.parse(new Date()) + endTime.getTime() * 60 * 1000);
-initializeClock('clockdiv', deadline);
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
 
-// halaman akan tersubmit otomatis pada 1 jam 30 menit kedepan
-window.setTimeout(function() {
-        document.getElementById("submit").click();
-    }, 5400000);
+      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+
+    updateClock();
+    const timeinterval = setInterval(updateClock, 1000);
+  }
+
+  const endTime = new Date(parseInt(document.getElementById('clockdiv').getAttribute('data-end-time')));
+  const deadline = new Date(Date.parse(new Date()) + endTime.getTime() * 60 * 1000);
+  initializeClock('clockdiv', deadline);
+
+  // halaman akan tersubmit otomatis pada 1 jam 30 menit kedepan
+  window.setTimeout(function() {
+    document.getElementById("submit").click();
+  }, 5400000);
 </script>
 @endpush
